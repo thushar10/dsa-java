18f693ef4ef77a26c3b641a8933d8e1168c5c6d9
Thushar P Sujir <thushar.gokusujir@gmail.com>
2025-08-30 14:05:47 +0530

    longest common prefix

---
+++ src/test/java/my/dsa/arrays/easy/LongestCommonPrefixTest.java
@@ -0,0 +1,30 @@
+package my.dsa.arrays.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LongestCommonPrefixTest {
+
+
+  @Test
+  public void longestCommonPrefixTest() {
+    LongestCommonPreFix longestCommonPreFix = new LongestCommonPreFix();
+    assertEquals("fl", longestCommonPreFix.longestCommonPrefixBest(new String [] {"flower", "flow", "flight"}));
+    assertEquals("", longestCommonPreFix.longestCommonPrefixBest(new String [] {"dog","racecar","car"}));
+  }
+
+  @Test
+  public void longestCommonPrefixTestEmpty() {
+    LongestCommonPreFix longestCommonPreFix = new LongestCommonPreFix();
+    assertEquals("", longestCommonPreFix.longestCommonPrefixBest(new String [] {}));
+
+  }
+
+  @Test
+  public void longestCommonPrefixTestOneWord() {
+    LongestCommonPreFix longestCommonPreFix = new LongestCommonPreFix();
+    assertEquals("flower", longestCommonPreFix.longestCommonPrefixBest(new String [] {"flower"}));
+
+  }
+}
